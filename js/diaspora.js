69abab95197f055863841d4cc8f06bd6a932ee9d
skyone-wzw <2654431601@qq.com>
2021-01-10 20:19:37 +0800

    Site updated: 2021-01-10 20:19:37

--- js/diaspora.js
+++ js/diaspora.js
@@ -610,7 +610,7 @@ $(function() {
 					  repo: comment.data('r'),
 					  owner: comment.data('o'),
 					  admin: comment.data('a'),
-					  id: decodeURI(window.location.pathname),
+					  id: md5(window.location.pathname),
 					  distractionFreeMode: comment.data('d')
 					})
 					$(".comment").removeClass("link")
